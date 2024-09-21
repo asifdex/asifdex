9444203a8b421df3188cd37c5521fa0d3ea56df7
xashadulpo <xashadulasif@gmail.com>
2024-09-21 18:25:05 +0600

    setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/exp1.svg" />
+       
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <title>Matar</title>
+        <meta
+          name="description"
+          content=""
+        />
       </body>
     </Html>
-  )
+  );
 }
